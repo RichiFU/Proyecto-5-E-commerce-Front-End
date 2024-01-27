df63c08104cf1ec77412d0af78ec216e532ecb89
RichiFU <andresurizarfuentes@gmail.com>
2024-01-27 20:54:52 -0300

    deploy

--- src/config/axiosClient.jsx
+++ src/config/axiosClient.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-    baseURL: "http://localhost:8080"
+    baseURL: "https://backend-kh4q.onrender.com"
 })
 
 export default axiosClient;
